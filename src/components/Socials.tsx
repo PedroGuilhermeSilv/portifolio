3a6ae1e62b0aa82d868bd61cdd54b7ef6debfd4a
guilherme <vinespedro55@gmail.com>
2025-01-19 19:47:39 -0300

    fix: update Instagram link to profile card

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -4,7 +4,7 @@ import { RiInstagramFill } from "react-icons/ri"
 
 
 const socials = [
-    { icon: <RiInstagramFill />, path: "https://www.instagram.com/" },
+    { icon: <RiInstagramFill />, path: "https://www.instagram.com/devguiilherme/profilecard/?igsh=cXlsODNwMGhtYjcx" },
     { icon: <FaGithub />, path: "https://github.com/PedroGuilhermeSilv" },
     { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/pedro-guilherme-silva-moura-53519019b" },
 ]
